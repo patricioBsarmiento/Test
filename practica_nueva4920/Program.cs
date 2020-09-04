68f6e6c69014aee571e47261cf3b3e6babaffb26
patricio belin sarmiento <patriciobelinsarmiento@gmail.com>
2020-09-04 19:38:54 -0300

    Primer_commit

---
+++ practica_nueva4920/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_nueva4920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int i;
+            for (i=0; i<5; i++)
+            {
+                Console.WriteLine("Hola Mundo!");
+                Console.ReadLine();
+            }*/
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i + 1);
+
+            } while (i < 10);
+            Console.WriteLine(i);
+            Console.ReadLine();
+
+        }
+    }
+}
